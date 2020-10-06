b07cbfddab40d183a8792ca8edad9b2369c0449f
eteeeeeerminal <momentneet@gmail.com>
2020-10-06 12:51:07 +0900

    フォルダの整理。投票を一回だけできるように。

--- src/TopicBox.py
+++ src/TopicBox.py
@@ -6,14 +6,10 @@ from tkinter import ttk
 
 from pytchat import LiveChat
 
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logging.basicConfig(level=logging.DEBUG, filename="log.txt")
-
+from utils import *
 
 class TopicBox:
-    def __init__(self, config_dict):
+    def __init__(self, config_dict, is_test:bool=False):
         self.ui_config  = config_dict["UI_config"]
         self.box_config = config_dict["TopicBoxConfig"]
 
@@ -21,10 +17,15 @@ class TopicBox:
         self.votes_n = 0
         self.topics  = self.box_config["initial_topic"]
 
+        self.once_vote = True
+        self.voted_users = []
+
         self.create_ui()
         self.reset()
 
-        self.livechat = LiveChat(video_id=self.box_config["video_id"], callback=self.process_chat)
+        self.livechat = LiveChat(
+            video_id=self.box_config["video_id"], callback=self.process_chat
+        ) if not is_test else None
 
     def create_ui(self):
         self.root = tk.Tk()
@@ -39,8 +40,9 @@ class TopicBox:
         self.choice_dicts = []
         for i in range(self.box_config["topic_num"]):
             choice_frame = ttk.Frame(self.root, borderwidth=1, relief=tk.GROOVE)
-            choice_frame.pack(ipadx=5, ipady=5, expand=1, side=tk.TOP)
+            choice_frame.pack(ipadx=10, ipady=10, expand=1, side=tk.TOP)
 
+            # named tuple で書き換えかな
             line_d = {}
             tk.Label(choice_frame, text=f"No.{i+1}:", font=font)\
                 .pack(expand=1, side=tk.LEFT)
@@ -54,12 +56,13 @@ class TopicBox:
                 .pack(expand=1, side=tk.LEFT)
 
             line_d["is_check"] = tk.BooleanVar()
-            tk.Checkbutton(choice_frame, variable=line_d["is_check"])\
-                .pack(expand=1, side=tk.LEFT)
+            tk.Checkbutton(choice_frame, text="除外", variable=line_d["is_check"], indicatoron=False)\
+                .pack(padx=10, ipadx=10, ipady=10, expand=1, side=tk.LEFT)
 
             self.choice_dicts.append(line_d)
 
         # 1番下の部分
+        # named tuple で書き換えかな
         self.bottom = {}
         self.bottom["frame"] = ttk.Frame(self.root)
         self.bottom["frame"].pack(expand=1, fill=tk.BOTH, side=tk.TOP)
@@ -68,18 +71,45 @@ class TopicBox:
         self.bottom["start"] = tk.Button(self.bottom["frame"], command=self.start, text="Start", font=font, borderwidth=5)
         self.bottom["start"].pack(expand=1, fill=tk.BOTH, padx=5, pady=5, side=tk.LEFT)
 
-    def process_chat(self, chatdata):
-        for c in chatdata.items:
-            if "/add" in c.message:
-                self.topics.append(c.message.strip("/add").strip())
+    def update_votes_n(self):
+        self.bottom["all_votes_n"]["text"] = f"投票数:{self.votes_n}"
+
+    def add_topic(self, topic_text:str):
+        self.topics.append(topic_text)
+
+    def get_votes(self, votes:str):
+        for i in range(self.box_config["topic_num"]):
+            n = votes.count(str(i+1))
+            self.counter[i] += n
+            self.votes_n += n
+        self.update_votes_n()
+
+    def get_avote(self, vote:str, user:str):
+        if user in self.voted_users:
+            return
+
+        for i in range(self.box_config["topic_num"]):
+            if not str(i+1) in vote:
                 continue
+            self.counter[i] += 1
+            self.votes_n += 1
+            self.voted_users.append(user)
+            break
+        self.update_votes_n()
 
-            for i in range(self.box_config["topic_num"]):
-                n = c.message.count(str(i+1))
-                self.counter[i] += n
-                self.votes_n += n
+    def process_chat(self, chatdata):
+        # 先に chat list を作れば高階関数でできる?
+        # if else は配列を2つに分割して
+        for c in chatdata.items:
+            chat = c.message
+            if "/add" in chat:
+                self.add_topic(chat.strip("/add").strip())
 
-            self.bottom["all_votes_n"]["text"] = f"投票数:{self.votes_n}"
+            else:
+                if self.once_vote:
+                    self.get_avote(chat, c.author.channelId)
+                else:
+                    self.get_votes(chat)
 
     def stop(self):
         for i in range(self.box_config["topic_num"]):
@@ -90,11 +120,11 @@ class TopicBox:
 
     def start(self):
         for i in range(self.box_config["topic_num"]):
-            if(self.box_config["topic_num"] >= len(self.topics)):
-                continue
-
             if(self.choice_dicts[i]["is_check"].get()):
-                del self.topics[i]
+                if(self.box_config["topic_num"] >= len(self.topics)):
+                    self.topics[i] = "ネタ切れ"
+                else:
+                    self.topics.pop(i)
 
         self.reset()
 
@@ -104,6 +134,7 @@ class TopicBox:
     def reset(self):
         self.counter = [0] * self.box_config["topic_num"]
         self.votes_n = 0
+        self.voted_users = []
 
         random.shuffle(self.topics)
 
@@ -112,8 +143,9 @@ class TopicBox:
             self.choice_dicts[i]["votes_n"].set("???")
             self.choice_dicts[i]["is_check"].set(False)
 
-        self.bottom["all_votes_n"]["text"] = f"投票数:{self.votes_n}"
+        self.update_votes_n()
 
     def main(self):
         self.root.mainloop()
-        self.livechat.terminate()
+        if self.livechat is not None:
+            self.livechat.terminate()
---
+++ src/utils.py
@@ -0,0 +1,5 @@
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, filename="log.txt")
+
